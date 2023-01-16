40e8a850469110f4e844c8b63519058382565d32
Ulfet1987 <ulfet.imameliyev@gmail.com>
2023-01-15 21:42:44 -0500

    Initial commit

---
+++ src/test/java/pojos/PetStoreUserResponsePojo.java
@@ -0,0 +1,52 @@
+package pojos;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PetStoreUserResponsePojo {
+    private Integer code;
+    private String type;
+    private String message;
+
+    public PetStoreUserResponsePojo(Integer code, String type, String message) {
+        this.code = code;
+        this.type = type;
+        this.message = message;
+    }
+
+    public PetStoreUserResponsePojo() {
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "PetStoreUserResponsePojo{" +
+                "code=" + code +
+                ", type='" + type + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
